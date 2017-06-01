2287a0e4c5b1a3374e7f6d600df660d0ac944a3f
test <kvivaldi@gmail.com>
2017-06-01 14:18:44 +0900

    Frist commit

---
+++ WebContent/pushState/page.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+	$(document).on('click', '.control nav a', function(event) {
+		//history.pushState(null, null, event.target.href);
+		
+		var obj = {name:"sam", href:event.target.href};
+		
+		history.pushState(obj, "testPage", event.target.href);
+		
+		$('article').load(event.target.href + ' article>.content');
+		
+		event.preventDefault();
+
+	});
+
+	$(window).on('popstate', function(event) {
+		console.log(event);
+		console.log("location.href : " + location.href );
+		$('article').load(location.href + ' article>.content');
+	});
+
+	var audio = new Audio('/Study/pushState/music1.mp3');
+	
+	$(document).on('click', '.control .player .play', function(event) {
+		audio.play();
+		event.preventDefault();
+	});
+
+	$(document).on('click', '.control .player .pause', function(event) {
+		audio.pause();
+		event.preventDefault();
+	});
+	
+});
+
